20a58b37d2cf14bec1a2bbb70abb3c5ec86a0804
LouisSzeto <hke0073@icloud.com>
2022-07-18 23:36:50 +0800

    New api endpoint, `BaseDataCollection` abstraction, demo algos

--- process.py
+++ process.py
@@ -1,80 +1,127 @@
 import json
 import pathlib
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
 import requests
+import sys
 
-url = os.environ["REGALYTICS_API_BASE_URL"] + "/all"
-
-payload = json.dumps({
-    "apikey": os.environ["REGALYTICS_API_KEY"]
-})
-headers = {
+URL = os.environ["REGALYTICS_API_BASE_URL"]
+HEADERS = {
     'Content-Type': 'application/json'
 }
+ARTICLE_PATH = pathlib.Path('/temp-output-directory/alternative/regalytics/articles')
 
-response = requests.post(url, headers=headers, data=payload).json()
-articles_path = pathlib.Path('/temp-output-directory/alternative/regalytics/articles')
-# objectives:# download data from API -> temp folder or in memory. Output processed datat  to /temp-output-directory/alternative/regalytics/articles/yyyyMMdd.json
-articles_path.mkdir(parents=True, exist_ok=True)
-# "states": [
-#         {
-#           "name": "United States",
-#           "country": {
-#             "name": "United States"
-#           }
-#         }
-#       ],
-# if states is more than 0
-# loop into state and get the state name
-# 1. query all data, -> /api/.../all; 2. look at latest_update, add delta of 1/2 days;
-# 3. write data to date of latest_update + delta. This date must be on the date we published the article on Regalytics
-articles = response['articles']
-articles_by_date = {}
+def main(date):
+    # objectives:# download data from API -> temp folder or in memory. Output processed datat  to /temp-output-directory/alternative/regalytics/articles/yyyyMMdd.json
+    ARTICLE_PATH.mkdir(parents=True, exist_ok=True)
+    articles_by_date = {}
 
-for article in articles:
-    article['in_federal_register'] = 'yes' in article['in_federal_register'].lower()
-    # State -> Dictionary<string, List<string>>
-    states = {}
-    if 'states' not in article or article['states'] is None:
-        continue
+    if date == "all":
+        url = f"{URL}/get-all"
+        payload = json.dumps({
+            "apikey": os.environ["REGALYTICS_API_KEY"]
+        })
+        
+        response = requests.post(url, headers=HEADERS, data=payload).json()
+        max_page = response['all_pages']
+        articles = response['results']
 
-    for state in article['states']:
-        if 'country' not in state:
-            continue
-
-        country = state['country']
-        if country is None:
-            continue
-
-        if not country['name'] in states:
-            country_states = []
-            states[country['name']] = country_states
-        else:
-            country_states = states[country['name']]
-
-        country_states.append(state['name'])
-
-    article['states'] = states
-    article['agencies'] = [agency['name'] for agency in article['agencies']]
-    
-    date = datetime.strptime(article['latest_update'], '%Y-%m-%d')
-    date_key = date.strftime('%Y%m%d')
-
-    if date_key not in articles_by_date:
-        date_articles = []
-        articles_by_date[date_key] = date_articles
+        for i in range(2, max_page + 1):
+            response = requests.post(f'{url}?page={i}', headers=HEADERS, data=payload).json()
+            articles += response['results']
+            
     else:
-        date_articles = articles_by_date[date_key]
+        url = f"{URL}/search"
+        payload = json.dumps({
+            "apikey": os.environ["REGALYTICS_API_KEY"],
+            "search_options": {
+                "created_at": {
+                    "start": date,
+                    "end": date
+                }
+            }
+        })
+        
+        response = requests.post(url, headers=HEADERS, data=payload).json()
+        articles = response['articles']
+        
+    # "agencies": [
+    #     {
+    #         "name": "Iowa Department of Human Services",
+    #         "states": [
+    #             {
+    #                 "name": "Iowa"
+    #             }
+    #         ],
+    #         "countries": [
+    #             {
+    #                 "name": "United States"
+    #             }
+    #         ]
+    #     }
+    # ]
+    # if states is more than 0
+    # loop into state and get the state name
+    # 1. query all data, -> /api/v2/.../get-all; 2. look at latest_update, add delta of 1/2 days;
+    # 3. write data to date of latest_update + delta. This date must be on the date we published the article on Regalytics
 
-    date_articles.append(article)
-
-for date, articles in articles_by_date.items():
-    lines = []
     for article in articles:
-        lines.append(json.dumps(article, indent=None))
+        article['in_federal_register'] = 'yes' in article['in_federal_register'].lower()
+        # State -> Dictionary<string, List<string>>
+        states = {}
+        for agency in article['agencies']:
+            state = agency['states']
+            
+            if 'states' not in agency or state is None:
+                continue
 
-    article_lines = '\n'.join(lines)
+            if 'countries' not in agency:
+                continue
 
-    with open(articles_path / f'{date}.json', 'w') as article_file:
-        article_file.write(article_lines)
+            countries = agency['countries']
+            if countries is None:
+                continue
+            
+            for country in countries:
+                name = country['name']
+                
+                if not name in states:
+                    country_states = []
+                    states[name] = country_states
+                else:
+                    country_states = states[name]
+
+            country_states.extend([x['name'] for x in state])
+
+        article['states'] = states
+        article['agencies'] = [agency['name'] for agency in article['agencies']]
+        
+        # remove timezone info (-04:00) [NewYork]
+        article['created_at'] = article['created_at'][:-6]
+        
+        # all data received during day T would confer into day T+1 00:00
+        date = datetime.strptime(article['created_at'], '%Y-%m-%dT%H:%M:%S.%f').date()
+        date_key = date.strftime('%Y%m%d')
+
+        if date_key not in articles_by_date:
+            date_articles = []
+            articles_by_date[date_key] = date_articles
+        else:
+            date_articles = articles_by_date[date_key]
+
+        date_articles.append(article)
+
+    for date, articles in articles_by_date.items():
+        lines = []
+        for article in articles:
+            lines.append(json.dumps(article, indent=None))
+
+        article_lines = '\n'.join(lines)
+
+        with open(ARTICLE_PATH / f'{date}.json', 'w') as article_file:
+            article_file.write(article_lines)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise ValueError("process.py only takes 1 argument.")
+    main(sys.argv[-1])
